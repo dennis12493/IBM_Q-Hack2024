181fe81e2e1769eaef261564c8888a7f64a4c4f3
dennis124 <dennisuntiet664@gmail.com>
2024-04-03 16:43:41 +0200

    video iframe

---
+++ frontend/src/lib/Video.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let url: string = "";
+    function changeIframe(event: Event) {
+        url = (event.target as HTMLInputElement).value;
+    }
+</script>
+
+<div>
+    <div>
+        <input on:change={changeIframe} />
+        <iframe
+            width="560"
+            height="315"
+            src={url}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+        ></iframe>
+    </div>
+</div>
+
+<style>
+    div {
+        background-color: var(--background);
+    }
+</style>
