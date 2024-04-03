9a5b59d381fe47fdb0a22cd8b2690ff03aa18cda
madebytimo <t.schnaible@madebytimo.de>
2024-04-03 23:23:10 +0200

    update videos, add transcripts

--- frontend/src/lib/Video.svelte
+++ frontend/src/lib/Video.svelte
@@ -4,7 +4,7 @@
     let urls: string[] = [
         "https://www.youtube.com/embed/v6tUWk7vC6g",
         "https://www.youtube.com/embed/uipjCTg_PqQ",
-        "https://www.youtube.com/embed/kV1ru-Inzl4",
+        "https://www.youtube.com/embed/NiKtZgImdlY",
     ];
     $: $selectedVideo, handleVideoChange();
 
