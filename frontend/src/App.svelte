3c5611063109e7c5a30b3c21e40a0771fdd9d75a
dennis124 <dennisuntiet664@gmail.com>
2024-04-03 15:49:41 +0200

    added file dropzone

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+    import Video from "./lib/Video.svelte";
+
 </script>
 
 <main>
     <p>Hier ist unsere krasse App</p>
     <p>Vielleicht hier nur die route Components hin pls</p>
+    <Video/>
 </main>
 
 <style>
