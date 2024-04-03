4cdbf3eff1446a895fd2906b01b194e6bee26747
dennis124 <dennisuntiet664@gmail.com>
2024-04-03 17:34:16 +0200

    video component

--- frontend/src/lib/Video.svelte
+++ frontend/src/lib/Video.svelte
@@ -1,27 +1,23 @@
 <script lang="ts">
     let url: string = "";
-    function changeIframe(event: Event) {
-        url = (event.target as HTMLInputElement).value;
-    }
 </script>
 
 <div>
-    <div>
-        <input on:change={changeIframe} />
-        <iframe
-            width="560"
-            height="315"
-            src={url}
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-        ></iframe>
-    </div>
+    <iframe
+        src={url}
+        height="400rem"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+    ></iframe>
 </div>
 
 <style>
     div {
         background-color: var(--background);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
 </style>
