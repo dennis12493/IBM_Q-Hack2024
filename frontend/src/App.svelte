b612d45a8ef5b95170472e554d06788820c0d6d7
dennis124 <dennisuntiet664@gmail.com>
2024-04-03 17:39:24 +0200

    first navigation

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-    import Video from "./lib/Video.svelte";
+    import Navigation from "./lib/Navigation.svelte";
+import Video from "./lib/Video.svelte";
 
 </script>
 
 <main>
-    <p>Hier ist unsere krasse App</p>
-    <p>Vielleicht hier nur die route Components hin pls</p>
+    <Navigation/>
     <Video/>
 </main>
 
 <style>
+    main {
+        display: flex;
+        flex-direction: row;
+    }
 </style>
---
+++ frontend/src/lib/Navigation.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div>
+    <button>Die Biene</button>
+    <button>Der Baum</button>
+    <button>Die Tanne</button>
+</div>
+
+<style>
+    div {
+        background-color: var(--background);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+</style>
