ca049255516bfd34f9b09eac0d15554363577093
Julius Thaller <thallerjulius@gmail.com>
2024-04-03 22:10:10 +0200

    added layout elements

--- frontend/src/pages/TeacherPage.svelte
+++ frontend/src/pages/TeacherPage.svelte
@@ -1,16 +1,27 @@
 <script lang="">
-    import TeacherHeader from "../lib/TeacherComponents/TeacherHeader.svelte";
+    import ClassView from "../lib/TeacherComponents/ClassView.svelte";
+import StudentView from "../lib/TeacherComponents/StudentView.svelte";
+import TeacherHeader from "../lib/TeacherComponents/TeacherHeader.svelte";
 
 
 </script>
 
 <div class="teacher-wrapper">
     <TeacherHeader></TeacherHeader>
+    <div class="teacher-body">
+        <StudentView></StudentView>
+        <ClassView></ClassView>
+    </div>
 </div>
 
 <style>
     .teacher-wrapper{
         background-color: var(--background);
         height: 100%;
+    }
+
+    .teacher-body{
+        display: flex;
+        height: calc(100% - 4rem);
     }
 </style>
